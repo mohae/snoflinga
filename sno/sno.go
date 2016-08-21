b1fc301b3525ea67dd756ee588909f32a520d516
Joel Scoble <joel.scoble@outlook.com>
2016-08-20 19:44:15 -0500

    add sno: a snowflake implementation that uses milliseconds and supports a 13-bit secondary id

---
+++ sno/sno.go
@@ -0,0 +1,119 @@
+// Package snö generates snowflake like 128 bit ids that uses milliseconds
+// for the time elemennt and supports a secondary 13-bit binary data element.
+// If there is the possibility of needing more than 1 million IDs per second
+// for a given ID and secondary ID combination this package should not be used.
+//
+// Data layout:
+//    0-40    time, in milliseconds: 69 years
+//   41-50    sequence; for collision prevention
+//   51-63    secondary id
+//  64-127    primary ID
+package sno
+
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"sync/atomic"
+	"time"
+)
+
+const (
+	idBits              = 64
+	sidBits             = 13
+	sequenceBits        = 9
+	sequenceMax  uint64 = 1<<sequenceBits - 1
+	sidMask      int16  = -1 ^ (-1 << (sidBits % 16))
+)
+
+// Flake is the type for a 128 bit snowflake.
+type Flake [16]byte
+
+// Generator creates snowflakes for a given id
+type Generator struct {
+	id       []byte
+	sid      int16
+	sequence uint64
+}
+
+// New returns an initialized generator.  If the passed byte slice is
+// greater than 8 bytes, the first 8 bytes will be used for the generator's id.
+// If the passed byte slice is less than 8 bytes, the id will be left-padded
+// with 0, zero.  The id2 parameter is the secondary id: only the right-most
+// 12 bits are used.  The generator's sequence is initialized with a random
+// number.
+func New(id []byte, id2 int16) Generator {
+	var g Generator
+	if len(id) < 8 {
+		g.id = make([]byte, 8-len(id))
+	}
+	if len(id) > 8 {
+		id = id[:8]
+	}
+	g.id = append(g.id, id...)
+	g.sid = id2 & sidMask
+	g.sequence = seed()
+	return g
+}
+
+// Snowflake generates an Flake from the current time and next sequence value.
+func (g *Generator) Snowflake() Flake {
+	var flake Flake
+	now := uint64(time.Now().UnixNano() / 1000000)
+	v := atomic.AddUint64(&g.sequence, 1)
+	v = v % sequenceMax
+	flake[0] = byte(now >> 33)
+	flake[1] = byte(now >> 25)
+	flake[2] = byte(now >> 17)
+	flake[3] = byte(now >> 9)
+	flake[4] = byte(now >> 1)
+	flake[5] = byte(now<<7) | uint8(v>>3)
+	flake[6] = byte(v<<5) | byte(g.sid>>8)
+	flake[7] = byte(g.sid)
+	flake[8] = g.id[0]
+	flake[9] = g.id[1]
+	flake[10] = g.id[2]
+	flake[11] = g.id[3]
+	flake[12] = g.id[4]
+	flake[13] = g.id[5]
+	flake[14] = g.id[6]
+	flake[15] = g.id[7]
+	return flake
+}
+
+// ID returns the ID bytes associated with this generator.
+func (g *Generator) ID() []byte {
+	return g.id
+}
+
+// SID returns the secondary ID associated with this generator.
+func (g *Generator) SID() int16 {
+	return g.sid
+}
+
+// Time returns the Flake's timestamp as an int64.  The timestamp has microsecond
+// resolution.
+func (f *Flake) Time() int64 {
+	return int64(f[0])<<33 | int64(f[1])<<25 | int64(f[2])<<17 | int64(f[3])<<9 | int64(f[4])<<1
+}
+
+// ID returns the Flake's ID as a []byte.  A snowflake's ID is the last 8
+// bytes.  No assumptiosn are made about either the contents of those bytes
+// or their layout: that is left up to the user.
+func (f *Flake) ID() []byte {
+	return f[8:]
+}
+
+// SID returns the Flake's secondary ID.  The secondary ID is 13 bits.
+func (f *Flake) SID() int16 {
+	return int16(f[6]<<3>>3)<<8 | int16(f[7])
+}
+
+func seed() uint64 {
+	bi := big.NewInt(int64(sequenceMax) + 1)
+	r, err := rand.Int(rand.Reader, bi)
+	if err != nil {
+		panic(fmt.Sprintf("entropy read error: %s", err))
+	}
+	return r.Uint64()
+}
---
+++ sno/sno_test.go
@@ -0,0 +1,67 @@
+package sno
+
+import (
+	"bytes"
+	"testing"
+	"time"
+)
+
+func TestNew(t *testing.T) {
+	tests := []struct {
+		id          []byte
+		sid         int16
+		expectedID  []byte
+		expectedSID int16
+	}{
+		{nil, 0, []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, 0},
+		{nil, 1, []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, 1},
+		{[]byte{}, 0, []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, 0},
+		{[]byte{}, 11, []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, 11},
+		{[]byte(""), 0, []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, 0},
+
+		{[]byte(""), 42, []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, 42},
+		{[]byte("hello"), 0, []byte{0x00, 0x00, 0x00, 0x68, 0x65, 0x6c, 0x6c, 0x6f}, 0},
+		{[]byte("hello"), 127, []byte{0x00, 0x00, 0x00, 0x68, 0x65, 0x6c, 0x6c, 0x6f}, 127},
+		{[]byte("hello123"), 0, []byte{0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x31, 0x32, 0x33}, 0},
+		{[]byte("hello123"), 202, []byte{0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x31, 0x32, 0x33}, 202},
+
+		{[]byte("hello123"), 9403, []byte{0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x31, 0x32, 0x33}, 1211}, //0x24 0xbb > 0x04 0xbb
+		{[]byte("hello123abc"), 0, []byte{0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x31, 0x32, 0x33}, 0},
+		{[]byte("hello123abc"), 255, []byte{0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x31, 0x32, 0x33}, 255},
+		{[]byte("hello123abc"), 14923, []byte{0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x31, 0x32, 0x33}, 6731}, //0x3a 0x4b > 0x1a 0x4b
+	}
+	for i, test := range tests {
+		g := New(test.id, test.sid)
+		if bytes.Compare(g.id, test.expectedID) != 0 {
+			t.Errorf("%d: got %vl want %v", i, g.id, test.expectedID)
+		}
+		if g.sid != test.expectedSID {
+			t.Errorf("%d: got %d; want %d", i, g.sid, test.expectedSID)
+		}
+	}
+}
+
+func TestFlake(t *testing.T) {
+	id := "hello123"
+	sid := int16(1211)
+	g := New([]byte(id), sid)
+	for i := 0; i < 10; i++ {
+		// will just check against the second; assume that the milliseconds portion
+		// is ok
+		now := time.Now().Unix()
+		f := g.Snowflake()
+		if f.Time()/1000 != now {
+			// if not equal, test against the next second; just in case a second boundary was passed
+			if f.Time()/1000 != now+1 {
+				t.Errorf("got %d; want %d", f.Time()/1000, now)
+			}
+			continue
+		}
+		if string(f.ID()) != id {
+			t.Errorf("got %s, want %s", string(f.ID()), id)
+		}
+		if f.SID() != sid {
+			t.Errorf("got %d, want %d", f.SID(), sid)
+		}
+	}
+}
